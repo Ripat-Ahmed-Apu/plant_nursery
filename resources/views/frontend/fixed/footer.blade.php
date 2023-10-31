691f0bd686ffce0c0a52a153f56fed70afa6193b
Ripat Ahmed Apu <ahmed.ripat.apu@gmail.com>
2023-10-31 22:56:26 +0600

    first commit

---
+++ resources/views/frontend/fixed/footer.blade.php
@@ -0,0 +1,134 @@
+<div class="footer">
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">About Us</h2>
+
+                <div class="widget-inner">
+
+                    <p></p>
+
+                    <p>Place: Sector-10,Uttara,Dhaka</p>
+                    <p>Zip Code: 1234</p>
+
+                    <p>Phone:01758690300</p>
+
+                    <p>Email: <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="93fef2faffd3eafce6e1f7fcfef2fafdbdf0fcfe">RipatApu@gmail.com</a></p>
+
+                    <!-- <img src="images/payment.png" alt="we accept"> -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Information</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a >Frequently Asked Question</a></li>
+
+                        <li><a >Terms and Condition</a></li>
+
+                        <li><a >Privacy Policy</a></li>
+
+                        <li><a >Customer Service</a></li>
+
+                        <li><a>Delivery Information</a></li>
+
+                        <!-- <li><a href="">Manufacturers</a></li> -->
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Customer Care</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a>Contact Us</a></li>
+                        <p>Phone:01758690300</p>
+
+                        <p>Email: <a>RipatApu@gmail.com</a></p>
+                        <li><a>Customer Support </a></li>
+                        <!-- <li><a href="">Sitemap</a></li>
+
+                        <li><a href="">Gift Vouchers</a></li>
+
+                        <li><a href="">Live Chat 24x7</a></li> -->
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Our Services</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a>Shipping & Returns</a></li>
+
+                        <li><a>Secure Buying</a></li>
+
+                        <li><a>Contact</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+
+<!-- <div class="copyright text-center">
+
+<div class="container">
+
+    <div class="row"> -->
+
+        <!-- <p class="copyright"> Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+        </p> -->
+
+    <!-- </div>
+
+</div> -->
+
+<!-- </div> -->
